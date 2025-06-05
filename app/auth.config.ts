32b09cb29ded54129cfce278c555862262521cab
Luka <glukec4@gmail.com>
2025-06-05 13:24:17 +0200

    fix mongo/next-auth dependancies

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -1,9 +1,12 @@
 import { NextAuthConfig } from 'next-auth';
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import client from "@/lib/mongodb"
 
 export const authConfig = {
   pages: {
     signIn: '/login',
   },
+  //adapter: MongoDBAdapter(client),
   providers: [
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,34 +1,48 @@
-import { MongoClient } from "mongodb";
 import bcrypt from "bcryptjs";
 import { User } from "types/user";
-
-const uri = process.env.MONGODB_URI!;
-const options = { appName: "finance" };
-
-let clientPromise: Promise<MongoClient>;
-
-declare global {
-  // Allow global caching in dev mode
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
+import { MongoClient, ServerApiVersion } from "mongodb"
+ 
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
-
-if (!global._mongoClientPromise) {
-  const client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect();
+ 
+const uri = process.env.MONGODB_URI
+const options = {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 }
-
-clientPromise = global._mongoClientPromise;
-
-export default clientPromise;
+ 
+let client: MongoClient
+ 
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient
+  }
+ 
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options)
+  }
+  client = globalWithMongo._mongoClient
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options)
+}
+ 
+// Export a module-scoped MongoClient. By doing this in a
+// separate module, the client can be shared across functions.
+export default client
 
 export async function getUser(email: string){
-  const client = await clientPromise;
   const db = client.db('finance-test');
   return db.collection<User>("users").findOne({ email });
 }
 
 export async function createUser(email: string, password: string) {
-  const client = await clientPromise;
   const db = client.db('finance-test');
   const hashedPassword = await bcrypt.hash(password, 10);
   return await db.collection("users").insertOne({ email, password: hashedPassword }); 
