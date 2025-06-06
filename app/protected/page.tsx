e8f4b21036215ef3c82fae7f2cdf5d6f4a777fe8
Luka <glukec4@gmail.com>
2025-06-06 09:33:41 +0200

    up fix 2

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,7 +1,7 @@
 import { auth, signOut } from 'app/auth';
 
 export default async function ProtectedPage() {
-  let session = await auth();
+  const session = await auth();
   console.log('session123', session)
 
   return (
