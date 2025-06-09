cbcd031345dea4343b65d356660ab8ab832742a2
Luka <glukec4@gmail.com>
2025-06-09 09:20:11 +0200

    fix prod db

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,39 +1,3 @@
-/* import { MongoClient } from "mongodb";
-import bcrypt from "bcryptjs";
-import { User } from "types/user";
-
-const uri = process.env.MONGODB_URI!;
-const options = { appName: "finance" };
-
-declare global {
-  // Allow global caching in dev mode
-  var _mongoClientPromise: Promise<MongoClient> | undefined;
-}
-
-if (!global._mongoClientPromise) {
-  const client = new MongoClient(uri, options);
-  global._mongoClientPromise = client.connect();
-}
-
-const clientPromise: Promise<MongoClient> = global._mongoClientPromise;
-
-export default clientPromise;
-
-export async function getUser(email: string){
-  const client = await clientPromise;
-  const db = client.db('finance-test');
-  return db.collection<User>("users").findOne({ email });
-}
-
-export async function createUser(email: string, password: string) {
-  const client = await clientPromise;
-  const db = client.db('finance-test');
-  const hashedPassword = await bcrypt.hash(password, 10);
-  return await db.collection("users").insertOne({ email, password: hashedPassword }); 
-}
- */
-
-
 import bcrypt from "bcryptjs";
 import { User } from "types/user";
 import { MongoClient, ServerApiVersion } from "mongodb"
@@ -41,7 +5,9 @@ import { MongoClient, ServerApiVersion } from "mongodb"
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
 }
- 
+
+const dbName = process.env.NODE_ENV === 'production' ? 'finance' : 'finance-test';
+
 const uri = process.env.MONGODB_URI
 const options = {
   serverApi: {
@@ -70,12 +36,12 @@ if (process.env.NODE_ENV === "development") {
 export default client
 
 export async function getUser(email: string) {
-  const db = client.db('finance-test');
+  const db = client.db(dbName);
   return db.collection<User>("users").findOne({ email });
 }
 
 export async function createUser(email: string, password: string) {
-  const db = client.db('finance-test');
+  const db = client.db(dbName);
   const hashedPassword = await bcrypt.hash(password, 10);
   return db.collection("users").insertOne({ email, password: hashedPassword });
 }
